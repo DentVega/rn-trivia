a4d94502036ccf94b6b8906a0fa9f916a8402762
Brian Vega <brian@tivelabs.com>
2024-10-17 18:39:08 -0400

    first commit

--- index.js
+++ index.js
@@ -6,4 +6,26 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import reducers from './src/redux/reducers';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = [thunk];
+
+export default function Main() {
+  const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(...middleware)),
+  );
+
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
